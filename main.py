7e5c7d0e9a4b0eb941fb53833b20503a8f741ca2
christophejacques <cjacques.programmes@numericable.fr>
2023-11-02 22:56:53 +0100

    Ajout de commentaires

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ def fprint(*args, **kwargs):
 
 if __name__ == "__main__":   
     
-    # Get the database
+    # Connexion a mongoDB
     db = MongoDB()
+
+    # Connexion a Elastic
     es = Elastic()
 
+    # Selection de la Collection mongoDB tutoriel.cities
     db.use_database("tutoriel")
     db.use_collection("cities")
 
+    # reinitialisation de l'index Elastic cities
     if es.has_index("cities"):
         es.use_index("cities")
         es.delete_index()
@@ -22,11 +26,15 @@ if __name__ == "__main__":
         es.index = "cities"
     print()
 
+    # boucle sur la liste des documents de la Collection pour Ajout dans l'index
     for doc in db.find({}, {"_id": 0}):
         fprint(f"Add doc: {doc} .. ", end="")
         es.add_doc(doc)
 
+    # Force la mise a jour et l'enregistrement
     es.update_index()
     es.flush_index()
     print("Nombre de documents ajoutes :", es.count())
+
+    # Fermeture de la mongoDB
     es.close()
